81724496bd83045a2df2bcb3f4c26b9fce13f045
vijay <90137500+bijjamvijay@users.noreply.github.com>
2022-01-17 11:50:34 +0530

    Update IdeyLabs.jsx

--- IdeyLabs.jsx
+++ IdeyLabs.jsx
@@ -10,6 +10,7 @@ function IdeyLabs() {
         Password:"",
         Phone:"",
         date:"",
+        file:""
     
         
 
@@ -22,7 +23,7 @@ function IdeyLabs() {
     const handleSubmit=(e)=>{
         e.preventDefault();
        
-        const newData={id:data.id,first: data.first,Last: data.Last,Email :data.Email,Password: data.Password,Phone: data.Phone,date: data.date}
+        const newData={id:data.id,first: data.first,Last: data.Last,Email :data.Email,Password: data.Password,Phone: data.Phone,date: data.date,file:data.file}
        
         SetStore([...store, newData])
     }
@@ -43,26 +44,29 @@ function IdeyLabs() {
                       
                         <div className='form-group'>
                         <input type="text" placeholder='First Name'    onChange={handler}  required="text" maxlength="10" minlength="3" name='first'/> <br/>
-                        <small className='text-primary'>Name is required</small>
+                        <small className='text-success'>Name is required</small>
                         </div>
                         <div className='form-group'>
                           <input type="text" placeholder='Last Name' onChange={handler}  required="text"  maxlength="10" minlength="3"  name='Last' /> 
                         </div>
                         <div className='form-group'>
                         <input type="Email" placeholder='Please Enter Valid Email'  required="text"   className='form-control'onChange={handler}  name='Email'/>
-                        <small className='text-primary'>Please Enter vaild Email</small> </div>
+                        <small className='text-success'>Please Enter vaild Email</small> </div>
                          <div className='form-group'>
-                        <input type="password" placeholder='Please Enter Password'  required="text" className='form-control'onChange={handler} maxlength="10" minlength="3" name='Password' />
-                        <small className='text-primary'>Password Is Required</small>
+                        <input type="password" placeholder='Please Enter Password'  required="text" className='form-control'onChange={handler} maxlength="20" minlength="3" name='Password' />
+                        <small className='text-success'>Password Is Required</small>
                         </div>
                         <div className='form-group'>
                         <input type="text"  placeholder='Phone-Number'  maxlength="10" required="number" onChange={handler}  name='Phone' /> <br/>
-                        <small className='text-primary'>Password Is Required</small>
+                        <small className='text-success'>Password Is Required</small> </div>
                         <br/>
-                       
+                        <div className='form-group'>
+                        <input type="file"  onChange={handler} onChange={handler} required="file" name='file' /> <br/>
+                        <small className='text-success'>Please Upload Your Resume</small>
                         </div>
                         <div className='form-group'>
                         <input type="date" onChange={handler}  required="date" name='date' />
+                      
                         </div>
                         <div className='form-group'>
                         <button className='btn btn-success '>Submit</button>
@@ -94,6 +98,7 @@ function IdeyLabs() {
                        <th>Email</th>
                        <th>Password</th>
                        <th>Phone-Number</th>
+                       <th>File</th>
                        <th>Date</th>
                        
                        </tr>
@@ -107,7 +112,9 @@ function IdeyLabs() {
                            <td>{emp.Last}</td>
                            <td>{emp.Email}</td>
                            <td>{emp.Password}</td>
+                           
                            <td>{emp.Phone}</td>
+                           <td>{emp.file}</td>
                            <td>{emp.date}</td>
                            </tr>)
                        })}
