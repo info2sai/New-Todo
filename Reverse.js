049c49bd7f08ce3d7fd11c18cf91ad21c2d0968b
vijay <90137500+bijjamvijay@users.noreply.github.com>
2022-01-19 15:54:56 +0530

    Update Reverse.js

--- Reverse.js
+++ Reverse.js
@@ -1,6 +1,40 @@
-  let a="vijay";
-  let b=""
-   for(i=a.length-1;i>=0;i--){
-       b=b+a[i]
-   }
-   console.log(b)
+ // Reverse Method;
+
+ 
+
+(1) let a=[5,15,25,35,45,55];
+
+ let b=a.reverse();
+ console.log(b);
+ 
+ 
+(B) WITH WHILE-LOOP;
+
+(1)let name1= 'Vijay';
+
+ let newNamee = name1.split('');
+
+
+let b=[]
+let i = 0;
+while(newNamee.length){
+   b.push(newNamee.pop());
+   i++;
+}
+console.log(b.join(""));
+
+if(name1==b){
+   console.log("its is palnodrome");
+   
+}else{
+   console.log("no");
+   
+}
+
+// (c ) WITH FOR-LOOP
+let a="vijay";
+let b=""
+ for(i=a.length-1;i>=0;i--){
+     b=b+a[i]
+ }
+ console.log(b)
