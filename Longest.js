7753cf6e7cc91665e3e3127b21acdd0cd82b84c5
vijay <90137500+bijjamvijay@users.noreply.github.com>
2022-01-19 15:59:29 +0530

    Update Longest.js

--- Longest.js
+++ Longest.js
@@ -1,10 +1,17 @@
-// let a=[1,1,2,2,3,34,5,68];
+let a=[1,1,2,2,3,34,5,68];
 
 
-// let b=Math.max(a);
-// console.log(Math.min(1,2,3,4))
+let b=Math.max(a);
+console.log(Math.min(1,2,3,4))
 
 
-//  
+ 
 
 console.log(Math.min( 1,2,3,4,5,6,7,7,8))
+
+// With out using Math.max;
+let a=[0,2,4,1,15,4,5,7,8,2,1];
+   let b=[];
+   a.sort((c,d)=>c-d);
+   b=a[a.length-1]
+   console.log(b);
